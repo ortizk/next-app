f9e6a6433516f4e1c347458de0ece998bfa8d0a4
ortizk <ortizk@live.com>
2024-01-17 18:27:18 -0800

    fixed error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import AuthProvider from "./auth/Provider";
+import SessionProvider from "./auth/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="winter">
       <body className={inter.className}>
-        <AuthProvider>
+        <SessionProvider>
           <NavBar />
           <main className="p-5">{children}</main>
-        </AuthProvider>
+        </SessionProvider>
       </body>
     </html>
   );
